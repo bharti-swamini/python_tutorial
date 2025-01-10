58af4cfe1cc0af237cb14a213881257c1091dfc2
swamika <swamikab@gmail.com>
2025-01-10 22:56:20 +0530

    code push

---
+++ 01_intro_to_strings.py
@@ -0,0 +1,4 @@
+name = "honey"
+nameshort = name[0:3] #start from index 0 all the way till 3 (excluding 3)
+character1 = name[1]
+print(character1)
---
+++ str_functions.py
@@ -0,0 +1,8 @@
+name = "swamika"
+print(len(name))
+
+print(name.endswith("mika"))
+print(name.endswith("mikaa"))
+print(name.startswith("swami"))
+print(name.startswith("soami"))
+print(name.capitalize())
